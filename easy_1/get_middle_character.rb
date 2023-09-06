98922336635439a3f2cbd36fddcbb9d9d92b11f8
Jeffrey <echo41011@gmail.com>
2023-09-06 17:01:30 -0500

    Easy 1 problems

--- easy_1/get_middle_character.rb
+++ easy_1/get_middle_character.rb
@@ -1,12 +1,16 @@
 def center_of(phrase)
   letters = phrase.chars
-  if letters.size %2 == 1 then return letters[letters.size/ 2] end
-  if letters.size %2 == 0 then return letters[letters.size/ 2] + letters[letters.size/2+1] end
+  if letters.size() %2 == 1 then 
+    return letters[letters.size/ 2] 
+  end
+  if letters.size() %2 == 0 then
+     return letters[letters.size() / 2 - 1] + letters[letters.size()/2] 
+  end
 
 end
 
-p center_of('I love ruby') == 'e'
-p center_of('Launch School') == ' '
-p center_of('Launch') == 'un'
-p center_of('Launchschool') == 'hs'
-p center_of('x') == 'x'
+p center_of('I love ruby') #== 'e'
+p center_of('Launch School') #== ' '
+p center_of('Launch') #== 'un'
+p center_of('Launchschool') #== 'hs'
+p center_of('x') #== 'x'
